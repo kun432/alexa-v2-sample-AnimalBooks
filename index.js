0d2491536870616da153cfe99491211982ba093f
Kuniaki Shimizu <kun432.8d1w@gmail.com>
2018-09-02 22:30:32 +0900

    fix

--- index.js
+++ index.js
@@ -2,22 +2,12 @@ const Alexa = require('ask-sdk-core');
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
+      || (handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.name === 'AMAZON.HelpIntent');
   },
   handle(handlerInput) {
-    const speechText = 'アレクサ、アニマルブックスのおすすめの商品を教えて、と声をかけてください。';
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .getResponse();
-  },
-};
-
-const HelpHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
+    console.log(`Session start!!!`)
     const speechText = 'アレクサ、アニマルブックスのおすすめの商品を教えて、と声をかけてください。';
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -31,7 +21,33 @@ const RecommendIntentHandler = {
       && handlerInput.requestEnvelope.request.intent.name === 'RecommendIntent';
   },
   handle(handlerInput) {
-    const speechText = 'おすすめの商品はアレクサ開発ガイドです';
+    const speechText = '本のおすすめでよろしいですか？';
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    attributes.state = 'RecommendHandler';
+    handlerInput.attributesManager.setSessionAttributes(attributes);
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .getResponse();
+  },
+};
+
+const RecommendHandler = {
+  canHandle(handlerInput) {
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    const request = handlerInput.requestEnvelope.request;
+    return attributes.state === 'RecommendHandler' &&
+      request.type === 'IntentRequest' &&
+      (request.intent.name === 'AMAZON.YesIntent' || request.intent.name === 'AMAZON.NoIntent');
+  },
+  handle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request.intent;
+    let speechText;
+    if (request.name === 'AMAZON.YesIntent') {
+      speechText = 'おすすめの商品はアレクサ開発ガイドです';
+    } else {
+      speechText = 'おすすめの商品はアレクサです';
+    }
     return handlerInput.responseBuilder
       .speak(speechText)
       .getResponse();
@@ -79,10 +95,9 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
-    HelpHandler,
     RecommendIntentHandler,
     CancelAndStopIntentHandler,
-    SessionEndedRequestHandler,
+    SessionEndedRequestHandler
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
